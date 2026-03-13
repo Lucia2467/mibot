0266608550322e2c1a28ca956bb12a3998294187
Lucia2467 <luciabel479@gmail.com>
2026-03-13 13:26:36 -0400

    Add files via upload

--- social_tasks_routes.py
+++ social_tasks_routes.py
@@ -303,7 +303,7 @@ Rules:
 
     try:
         response = req.post(
-            f'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={api_key}',
+            f'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={api_key}',
             headers={'Content-Type': 'application/json'},
             json={
                 'contents': [{'parts': [{'text': prompt}]}],
